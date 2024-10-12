6f968791e6225cdf15f55c93a71cf5d6462c6ec5
VaNyA1593 <vankaboiko1593@gmail.com>
2024-10-12 18:09:09 +0100

    hi

--- bot.py
+++ bot.py
@@ -23,5 +23,10 @@ def coin(message):
     bot.reply_to(message, f""" {answer}
 """)
 
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
 
 bot.infinity_polling()
